45cda60eaad4f7036397d6c70e75f71f761d3410
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-10-29 18:13:59 +0200

    Add close function

--- programs/amm/src/util.rs
+++ programs/amm/src/util.rs
@@ -1,4 +1,7 @@
+use anchor_lang::__private::ErrorCode;
+use anchor_lang::__private::CLOSED_ACCOUNT_DISCRIMINATOR;
 use std::cell::RefMut;
+use std::io::Write;
 
 use crate::account::{Pool, Tick, Tickmap};
 use crate::decimal::Decimal;
@@ -143,6 +146,26 @@ pub fn price_to_tick_in_range(price: Decimal, low: i32, high: i32, step: i32) ->
         }
     }
     low.checked_mul(step).unwrap()
+}
+
+pub fn close<'info>(
+    info: AccountInfo<'info>,
+    sol_destination: AccountInfo<'info>,
+) -> ProgramResult {
+    // Transfer tokens from the account to the sol_destination.
+    let dest_starting_lamports = sol_destination.lamports();
+    **sol_destination.lamports.borrow_mut() =
+        dest_starting_lamports.checked_add(info.lamports()).unwrap();
+    **info.lamports.borrow_mut() = 0;
+
+    // Mark the account discriminator as closed.
+    let mut data = info.try_borrow_mut_data()?;
+    let dst: &mut [u8] = &mut data;
+    let mut cursor = std::io::Cursor::new(dst);
+    cursor
+        .write_all(&CLOSED_ACCOUNT_DISCRIMINATOR)
+        .map_err(|_| ErrorCode::AccountDidNotSerialize)?;
+    Ok(())
 }
 
 #[cfg(test)]
