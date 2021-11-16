0a89c031ec870cc4465d668b3b4eae0aa42c6693
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-11-16 20:17:58 +0100

    Add transferUsdcSol function

--- scripts/transfer.ts
+++ scripts/transfer.ts
@@ -1,6 +1,5 @@
-import * as anchor from '@project-serum/anchor'
-import { Provider } from '@project-serum/anchor'
-import { clusterApiUrl, Keypair, PublicKey } from '@solana/web3.js'
+import { BN, Provider } from '@project-serum/anchor'
+import { clusterApiUrl, Keypair, PublicKey, sendAndConfirmRawTransaction } from '@solana/web3.js'
 import { MOCK_TOKENS } from '@invariant-labs/sdk/src/network'
 import { MINTER } from './minter'
 import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
@@ -16,14 +15,37 @@ const connection = provider.connection
 const wallet = provider.wallet.payer as Keypair
 
 const main = async () => {
+  const recipient = new PublicKey('9ceXFTqXzJMFDzf9s3fQn2UZDV9ihD8TYRMygGNmSL6G')
+
+  // transferUsdcUsdt(recipient)
+  transferUsdcSol(recipient)
+}
+
+const transferUsdcUsdt = async (recipient: PublicKey) => {
   const usdc = new Token(connection, new PublicKey(MOCK_TOKENS.USDC), TOKEN_PROGRAM_ID, wallet)
   const usdt = new Token(connection, new PublicKey(MOCK_TOKENS.USDT), TOKEN_PROGRAM_ID, wallet)
-  const recipient = new PublicKey('6pkdfGhTFx25UPsFHG5P1uBhEPuDhuCdxhDrGB5bP8P5')
-  const recipientUsdc = await usdc.createAccount(recipient)
-  const recipientUsdt = await usdt.createAccount(recipient)
-  const amount = tou64(1000 * 10 ** 6)
 
-  await usdc.mintTo(recipientUsdc, MINTER, [], amount)
-  await usdt.mintTo(recipientUsdt, MINTER, [], amount)
+  const recipientUsdc = (await usdc.getAccountInfo(recipient)).address
+  const recipientUsdt = (await usdt.getAccountInfo(recipient)).address
+
+  const usdcAmount = new BN(10).pow(new BN(6)).muln(1000) // 1000 USDC
+  const usdtAmount = new BN(10).pow(new BN(6)).muln(1000) // 1000 USDT
+
+  await usdc.mintTo(recipientUsdc, MINTER, [], tou64(usdcAmount))
+  await usdt.mintTo(recipientUsdt, MINTER, [], tou64(usdtAmount))
 }
+const transferUsdcSol = async (recipient: PublicKey) => {
+  const usdc = new Token(connection, new PublicKey(MOCK_TOKENS.USDC), TOKEN_PROGRAM_ID, wallet)
+  const sol = new Token(connection, new PublicKey(MOCK_TOKENS.SOL), TOKEN_PROGRAM_ID, wallet)
+
+  const recipientUsdc = (await usdc.getAccountInfo(recipient)).address
+  const recipientSol = (await sol.getAccountInfo(recipient)).address
+
+  const usdcAmount = new BN(10).pow(new BN(6)).muln(1000) // 1000 USD
+  const solAmount = new BN(10).pow(new BN(9)).muln(5) // 5 SOL
+
+  await usdc.mintTo(recipientUsdc, MINTER, [], tou64(usdcAmount))
+  await sol.mintTo(recipientSol, MINTER, [], tou64(solAmount))
+}
+
 main()
