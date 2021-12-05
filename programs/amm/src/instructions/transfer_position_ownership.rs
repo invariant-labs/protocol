94c37390c0d9430aebba0ebd713c5599ab0cc209
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-12-05 11:32:58 +0100

    Fix transfer position ownership handler

--- programs/amm/src/instructions/transfer_position_ownership.rs
+++ programs/amm/src/instructions/transfer_position_ownership.rs
@@ -52,40 +52,50 @@ pub fn handler(ctx: Context<TransferPositionOwnership>, bump: u8, index: u32) ->
 
     let mut owner_list = ctx.accounts.owner_list.load_mut()?;
     let mut recipient_list = ctx.accounts.recipient_list.load_mut()?;
-    let mut new_position = ctx.accounts.new_position.load_init()?;
-    let mut removed_position = ctx.accounts.removed_position.load_mut()?;
+    let new_position = &mut ctx.accounts.new_position.load_init()?;
+    let removed_position = &mut ctx.accounts.removed_position.load_mut()?;
 
     owner_list.head -= 1;
     recipient_list.head += 1;
 
     // reassign all fields in new_position
     {
-        new_position.owner = *ctx.accounts.recipient.key;
-        new_position.pool = removed_position.pool;
-        new_position.id = removed_position.id;
-        new_position.liquidity = removed_position.liquidity;
-        new_position.lower_tick_index = removed_position.lower_tick_index;
-        new_position.upper_tick_index = removed_position.upper_tick_index;
-        new_position.fee_growth_inside_x = removed_position.fee_growth_inside_y;
-        new_position.tokens_owed_x = removed_position.tokens_owed_x;
-        new_position.tokens_owed_y = removed_position.tokens_owed_y;
-        new_position.bump = bump; // new bump
+        **new_position = Position {
+            owner: *ctx.accounts.recipient.key,
+            pool: *ctx.accounts.recipient.key,
+            id: removed_position.id,
+            liquidity: removed_position.liquidity,
+            lower_tick_index: removed_position.lower_tick_index,
+            upper_tick_index: removed_position.upper_tick_index,
+            fee_growth_inside_x: removed_position.fee_growth_inside_x,
+            fee_growth_inside_y: removed_position.fee_growth_inside_y,
+            seconds_per_liquidity_inside: removed_position.seconds_per_liquidity_inside,
+            tokens_owed_x: removed_position.tokens_owed_x,
+            tokens_owed_y: removed_position.tokens_owed_y,
+            last_slot: removed_position.last_slot,
+            bump, // assign new bump
+        };
     }
 
     // when removed position is not the last one
     if owner_list.head != index {
         let last_position = ctx.accounts.last_position.load_mut()?;
-        // reassign all fields in owner position list
-        removed_position.owner = last_position.owner;
-        removed_position.pool = last_position.pool;
-        removed_position.id = last_position.id;
-        removed_position.liquidity = last_position.liquidity;
-        removed_position.lower_tick_index = last_position.lower_tick_index;
-        removed_position.upper_tick_index = last_position.upper_tick_index;
-        removed_position.fee_growth_inside_x = last_position.fee_growth_inside_x;
-        removed_position.fee_growth_inside_y = last_position.fee_growth_inside_y;
-        removed_position.tokens_owed_x = last_position.tokens_owed_x;
-        removed_position.tokens_owed_y = last_position.tokens_owed_y;
+
+        **removed_position = Position {
+            owner: last_position.owner,
+            pool: last_position.pool,
+            id: last_position.id,
+            liquidity: last_position.liquidity,
+            lower_tick_index: last_position.lower_tick_index,
+            upper_tick_index: last_position.upper_tick_index,
+            fee_growth_inside_x: last_position.fee_growth_inside_x,
+            fee_growth_inside_y: last_position.fee_growth_inside_y,
+            seconds_per_liquidity_inside: last_position.seconds_per_liquidity_inside,
+            tokens_owed_x: last_position.tokens_owed_x,
+            tokens_owed_y: last_position.tokens_owed_y,
+            last_slot: last_position.last_slot,
+            bump: removed_position.bump, // stay with the same bump
+        };
     }
 
     Ok(())
