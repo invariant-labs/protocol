e44e3708ea75d74c7a1c8ecc3156680431105fbf
Mateusz Zając <matzayonc@gmail.com>
2021-12-08 17:11:17 +0100

    updated ticks

--- scripts/create-pool.ts
+++ scripts/create-pool.ts
@@ -15,7 +15,13 @@ const market = new Market(Network.DEV, provider.wallet, connection)
 const wallet = provider.wallet.payer as Keypair
 
 const main = async () => {
-  const feeTier = FEE_TIERS[0]
+  const market = await Market.build(Network.DEV, provider.wallet, connection)
+
+  await createUsdcUsdt(market)
+  await createUsdcSol(market)
+  await createUsdcSolPricier(market)
+}
+const createUsdcUsdt = async (market: Market) => {
   const pair = new Pair(new PublicKey(MOCK_TOKENS.USDC), new PublicKey(MOCK_TOKENS.USDT), feeTier)
 
   await market.create({
@@ -23,4 +29,27 @@ const main = async () => {
     signer: wallet
   })
 }
+const createUsdcSol = async (market: Market) => {
+  const pair = new Pair(new PublicKey(MOCK_TOKENS.USDC), new PublicKey(MOCK_TOKENS.SOL), feeTier)
+
+  await market.create({
+    pair,
+    signer: wallet,
+    initTick: 16000
+  })
+}
+const createUsdcSolPricier = async (market: Market) => {
+  const pair = new Pair(
+    new PublicKey(MOCK_TOKENS.USDC),
+    new PublicKey(MOCK_TOKENS.SOL),
+    FEE_TIERS[1]
+  )
+
+  await market.create({
+    pair,
+    signer: wallet,
+    initTick: 16000
+  })
+}
+
 main()
