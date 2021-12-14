00253e72fb436d93ca89dab1b4fbc80d95b6df9b
Mateusz Zając <matzayonc@gmail.com>
2021-12-14 19:57:56 +0100

    wrote test testing deposits close to the limit

--- tests/limits.spec.ts
+++ tests/limits.spec.ts
@@ -8,23 +8,26 @@ import {
   createUserWithTokens
 } from './testUtils'
 import { Market, DENOMINATOR, Network } from '@invariant-labs/sdk'
-import { fromFee } from '@invariant-labs/sdk/src/utils'
+import { fromFee, toDecimal } from '@invariant-labs/sdk/src/utils'
 import { Decimal } from '@invariant-labs/sdk/src/market'
 import { Pair } from '@invariant-labs/sdk/src'
-import { getLiquidityByX } from '@invariant-labs/sdk/src/math'
+import { getLiquidityByX, getLiquidityByY } from '@invariant-labs/sdk/src/math'
+import { beforeEach } from 'mocha'
+import { assert } from 'chai'
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
 describe('limits', () => {
   const provider = Provider.local()
   const connection = provider.connection
   // @ts-expect-error
   const wallet = provider.wallet.payer as Keypair
-  let market: Market
   const protocolFee: Decimal = { v: fromFee(new BN(10000)) }
-
-  let knownPrice: Decimal
-  let expectedPrice: BN
+  let market: Market
+  let tokenX: Token
+  let tokenY: Token
   let pair: Pair
   let mintAuthority: Keypair
+  let knownPrice: Decimal = { v: new BN(DENOMINATOR) }
 
   before(async () => {
     market = await Market.build(
@@ -34,20 +37,30 @@ describe('limits', () => {
       anchor.workspace.Amm.programId
     )
     await market.createState(wallet, protocolFee)
+  })
 
+  beforeEach(async () => {
     const result = await createTokensAndPool(market, connection, wallet)
     pair = result.pair
     mintAuthority = result.mintAuthority
+
+    tokenX = new Token(connection, pair.tokenX, TOKEN_PROGRAM_ID, wallet)
+    tokenY = new Token(connection, pair.tokenY, TOKEN_PROGRAM_ID, wallet)
   })
 
-  it('big deposit', async () => {
-    const mintAmount = new BN(2).pow(new BN(64)).sub(new BN(1))
+  it('big deposit both tokens', async () => {
+    const mintAmount = new BN(2).pow(new BN(64)).subn(1)
     const { owner, userAccountX, userAccountY } = await createUserWithTokens(
       pair,
       connection,
       mintAuthority,
       mintAmount
     )
+
+    const liquidityByY = getLiquidityByY(mintAmount, -10, 10, { v: DENOMINATOR }, false).liquidity
+    const liquidityByX = getLiquidityByY(mintAmount, -10, 10, { v: DENOMINATOR }, false).liquidity
+    // calculation of liquidity might not be exactly equal on both tokens so taking smaller one
+    const liquidityDelta = liquidityByY.v.lt(liquidityByX.v) ? liquidityByY : liquidityByX
 
     market.initPosition(
       {
@@ -57,9 +70,83 @@ describe('limits', () => {
         userTokenY: userAccountY,
         lowerTick: -10,
         upperTick: 10,
-        liquidityDelta: getLiquidityByX(mintAmount, -10, 10, { v: DENOMINATOR }, false).liquidity
+        liquidityDelta
       },
       owner
     )
+  })
+
+  it('big deposit only X', async () => {
+    const mintAmount = new BN(2).pow(new BN(64)).subn(1)
+    const { owner, userAccountX, userAccountY } = await createUserWithTokens(
+      pair,
+      connection,
+      mintAuthority,
+      mintAmount
+    )
+
+    const lowerTick = 0
+    const upperTick = 10
+
+    const liquidityDelta = getLiquidityByX(
+      mintAmount,
+      lowerTick,
+      upperTick,
+      { v: DENOMINATOR },
+      true
+    ).liquidity
+
+    await market.initPosition(
+      {
+        pair,
+        owner: owner.publicKey,
+        userTokenX: userAccountX,
+        userTokenY: userAccountY,
+        lowerTick,
+        upperTick,
+        liquidityDelta
+      },
+      owner
+    )
+
+    assert.ok((await tokenX.getAccountInfo(userAccountX)).amount.eqn(0))
+    assert.ok((await tokenY.getAccountInfo(userAccountY)).amount.eq(mintAmount))
+  })
+
+  it('big deposit only Y', async () => {
+    const mintAmount = new BN(2).pow(new BN(64)).subn(1)
+    const { owner, userAccountX, userAccountY } = await createUserWithTokens(
+      pair,
+      connection,
+      mintAuthority,
+      mintAmount
+    )
+
+    const lowerTick = -10
+    const upperTick = 0
+
+    const liquidityDelta = getLiquidityByY(
+      mintAmount,
+      lowerTick,
+      upperTick,
+      { v: DENOMINATOR },
+      true
+    ).liquidity
+
+    await market.initPosition(
+      {
+        pair,
+        owner: owner.publicKey,
+        userTokenX: userAccountX,
+        userTokenY: userAccountY,
+        lowerTick,
+        upperTick,
+        liquidityDelta
+      },
+      owner
+    )
+
+    assert.ok((await tokenX.getAccountInfo(userAccountX)).amount.eq(mintAmount))
+    assert.ok((await tokenY.getAccountInfo(userAccountY)).amount.eqn(0))
   })
 })
