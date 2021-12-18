17fdb00923fc0f30c63acd46eb7cb4508eec89dc
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-12-18 14:35:53 +0100

    Fix decimal_to_x128 function & add tests

--- programs/amm/src/log.rs
+++ programs/amm/src/log.rs
@@ -1,11 +1,10 @@
 use crate::{decimal::Decimal, uint::U256};
 
-pub fn decimal_to_binary(decimal: Decimal) -> U256 {
-    // 2^128/10^12 = 340282366920938463463374607.431768211456
-    let w = U256::from("340282366920938463463374607431768211456");
+pub fn decimal_to_x128(decimal: Decimal) -> U256 {
+    let factor = U256::from(1) << 128;
 
     U256::from(decimal.to_u256())
-        .checked_mul(w)
+        .checked_mul(factor)
         .unwrap()
         .checked_div(Decimal::one().to_u256())
         .unwrap()
@@ -13,10 +12,26 @@ pub fn decimal_to_binary(decimal: Decimal) -> U256 {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::decimal::Decimal;
 
     #[test]
     fn test_decimal_to_binary() {
-        // test for max and min [sqrt_price]
-        println!("hello world!");
+        // max_sqrt_price = ceil(sqrt(2^64 - 1))
+        // min_sqrt_price = floor(sqrt_price(1/(2^64-1)))
+
+        let max_sqrt_price = Decimal::new(4294967295999999999884);
+        let min_sqrt_price = Decimal::new(329);
+
+        let max_sqrt_price_x128 = decimal_to_x128(max_sqrt_price);
+        let min_sqrt_price_x128 = decimal_to_x128(min_sqrt_price);
+
+        let expected_max_sqrt_price_x128 =
+            U256::from_dec_str("1461501637330902918164212078153454157894181088513").unwrap();
+        let expected_min_sqrt_price_x128 =
+            U256::from_dec_str("111952898716988754479450245845").unwrap();
+
+        assert!(max_sqrt_price_x128.eq(&expected_max_sqrt_price_x128));
+        assert!(min_sqrt_price_x128.eq(&expected_min_sqrt_price_x128));
     }
 }
