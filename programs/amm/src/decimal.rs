fbe8bea9e32bfcb4e528ef9a38b5148d1964751d
Mateusz Zając <matzayonc@gmail.com>
2021-12-30 21:04:00 +0100

    moved token amount to separe file

--- programs/amm/src/decimal.rs
+++ programs/amm/src/decimal.rs
@@ -1,3 +1,4 @@
+use crate::structs::token_amount::TokenAmount;
 use crate::uint::U256;
 use anchor_lang::prelude::*;
 use integer_sqrt::IntegerSquareRoot;
@@ -8,9 +9,6 @@ use std::{
 
 pub const SCALE: u8 = 12;
 pub const DENOMINATOR: u128 = 10u128.pow(SCALE as u32);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct TokenAmount(pub u64);
 
 #[derive(
     Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, AnchorDeserialize, AnchorSerialize,
@@ -42,6 +40,14 @@ impl Decimal {
         }
     }
 
+    pub fn is_zero(self) -> bool {
+        self.v == 0
+    }
+
+    pub fn from_token_amount(amount: TokenAmount) -> Decimal {
+        Decimal::from_integer(amount.0.into())
+    }
+
     pub fn div_up(self, other: Decimal) -> Decimal {
         Decimal::new(
             self.v
@@ -58,19 +64,20 @@ impl Decimal {
         Decimal::new(self.v.checked_mul(DENOMINATOR).unwrap().integer_sqrt())
     }
 
-    // This should return TokenAmount struct
-    pub fn to_token_floor(self) -> u64 {
-        self.v.checked_div(DENOMINATOR).unwrap().try_into().unwrap()
+    pub fn to_token_floor(self) -> TokenAmount {
+        TokenAmount(self.v.checked_div(DENOMINATOR).unwrap().try_into().unwrap())
     }
 
-    pub fn to_token_ceil(self) -> u64 {
-        self.v
-            .checked_add(DENOMINATOR.checked_sub(1).unwrap())
-            .unwrap()
-            .checked_div(DENOMINATOR)
-            .unwrap()
-            .try_into()
-            .unwrap()
+    pub fn to_token_ceil(self) -> TokenAmount {
+        TokenAmount(
+            self.v
+                .checked_add(DENOMINATOR.checked_sub(1).unwrap())
+                .unwrap()
+                .checked_div(DENOMINATOR)
+                .unwrap()
+                .try_into()
+                .unwrap(),
+        )
     }
 
     pub fn big_mul(self, other: Decimal) -> Decimal {
@@ -454,22 +461,22 @@ mod tests {
         {
             let d = Decimal::from_integer(1);
 
-            assert_eq!(d.to_token_floor(), 1);
-            assert_eq!(d.to_token_ceil(), 1);
+            assert_eq!(d.to_token_floor(), TokenAmount(1));
+            assert_eq!(d.to_token_ceil(), TokenAmount(1));
         }
         // little over
         {
             let d = Decimal::from_integer(1) + Decimal::new(1);
 
-            assert_eq!(d.to_token_floor(), 1);
-            assert_eq!(d.to_token_ceil(), 2);
+            assert_eq!(d.to_token_floor(), TokenAmount(1));
+            assert_eq!(d.to_token_ceil(), TokenAmount(2));
         }
         // little below
         {
             let d = Decimal::from_integer(2) - Decimal::new(1);
 
-            assert_eq!(d.to_token_floor(), 1);
-            assert_eq!(d.to_token_ceil(), 2);
+            assert_eq!(d.to_token_floor(), TokenAmount(1));
+            assert_eq!(d.to_token_ceil(), TokenAmount(2));
         }
     }
 
@@ -512,18 +519,18 @@ mod tests {
         {
             let a = Decimal::new(1);
             let b = Decimal::new(1);
-            assert_eq!(a.mul_up(b), Decimal::new(1));
+            assert_eq!(a.big_mul_up(b), Decimal::new(1));
         }
         // mul calculable without precision loss
         {
             let a = Decimal::from_integer(1);
             let b = Decimal::from_integer(3) / Decimal::new(10);
-            assert_eq!(a.mul_up(b), b);
+            assert_eq!(a.big_mul_up(b), b);
         }
         {
             let a = Decimal::from_integer(3) / Decimal::from_integer(10);
             let b = Decimal::new(3);
-            assert_eq!(a.mul_up(b), Decimal::new(1));
+            assert_eq!(a.big_mul_up(b), Decimal::new(1));
         }
         {
             let a = Decimal::new(2u128.pow(127) - 1);
