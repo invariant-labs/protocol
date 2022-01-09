b3c9e18f92bbbbbd20d55c2b240b1f3c35e59453
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-01-09 17:03:11 +0100

    Refactor

--- programs/amm/src/log.rs
+++ programs/amm/src/log.rs
@@ -9,16 +9,8 @@ use crate::{
 
 const LOG_SCALE: u128 = 64;
 const LOG_ONE: u128 = 1 << LOG_SCALE;
-
-pub fn decimal_to_x128(decimal: Decimal) -> U256 {
-    let factor = U256::from(1) << 128;
-
-    U256::from(decimal.to_u256())
-        .checked_mul(factor)
-        .unwrap()
-        .checked_div(Decimal::one().to_u256())
-        .unwrap()
-}
+const LOG_HALF: u128 = LOG_ONE >> 1;
+const LOG_DOUBLE: u128 = LOG_ONE << 1;
 
 pub fn decimal_to_x64(decimal: Decimal) -> u128 {
     decimal
@@ -31,92 +23,49 @@ pub fn decimal_to_x64(decimal: Decimal) -> u128 {
         .unwrap()
 }
 
-pub fn msb_x64(sqrt_price_x64: u128) -> u128 {
+pub fn log2_floor_x64(mut sqrt_price_x64: u128) -> u128 {
     let mut msb = 0;
-    let mut sqrt_price_x64 = sqrt_price_x64;
 
     if sqrt_price_x64 >= 1u128 << 64 {
         sqrt_price_x64 >>= 64;
-        msb += 64;
+        msb |= 64;
     };
     if sqrt_price_x64 >= 1u128 << 32 {
         sqrt_price_x64 >>= 32;
-        msb += 32;
+        msb |= 32;
     };
     if sqrt_price_x64 >= 1u128 << 16 {
         sqrt_price_x64 >>= 16;
-        msb += 16;
+        msb |= 16;
     };
     if sqrt_price_x64 >= 1u128 << 8 {
         sqrt_price_x64 >>= 8;
-        msb += 8;
+        msb |= 8;
     };
     if sqrt_price_x64 >= 1u128 << 4 {
         sqrt_price_x64 >>= 4;
-        msb += 4;
+        msb |= 4;
     };
     if sqrt_price_x64 >= 1u128 << 2 {
         sqrt_price_x64 >>= 2;
-        msb += 2;
+        msb |= 2;
     };
     if sqrt_price_x64 >= 1u128 << 1 {
-        msb += 1;
+        msb |= 1;
     };
 
     msb
 }
 
-pub fn msb_x128(sqrt_price_x128: U256) -> U256 {
-    let mut sqrt_price_x128 = sqrt_price_x128;
-    let mut log2_msb = U256::from(0);
-
-    if sqrt_price_x128 >= U256::from(1) << 128 {
-        sqrt_price_x128 >>= 128;
-        log2_msb += U256::from(128);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 64 {
-        sqrt_price_x128 >>= 64;
-        log2_msb += U256::from(64);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 32 {
-        sqrt_price_x128 >>= 32;
-        log2_msb += U256::from(32);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 16 {
-        sqrt_price_x128 >>= 16;
-        log2_msb += U256::from(16);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 8 {
-        sqrt_price_x128 >>= 8;
-        log2_msb += U256::from(8);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 4 {
-        sqrt_price_x128 >>= 4;
-        log2_msb += U256::from(4);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 2 {
-        sqrt_price_x128 >>= 2;
-        log2_msb += U256::from(2);
-    };
-    if sqrt_price_x128 >= U256::from(1) << 1 {
-        log2_msb += U256::from(1);
-    };
-
-    log2_msb
-}
-
 pub fn log2_x64(mut sqrt_price_x64: u128) -> (bool, u128) {
     let mut sign = true;
-    let scale = 1u128 << 64;
-    let scale_2x = scale << 1;
-    let half_scale = scale >> 1;
 
     // log2(x) = -log2(1/x), when x < 1
-    if sqrt_price_x64 < scale {
+    if sqrt_price_x64 < LOG_ONE {
         sign = false;
         // scale^2 / sqrt_price_x128
-        sqrt_price_x64 = U256::from(scale)
-            .checked_mul(U256::from(scale))
+        sqrt_price_x64 = U256::from(LOG_ONE)
+            .checked_mul(U256::from(LOG_ONE))
             .unwrap()
             .checked_div(U256::from(sqrt_price_x64 + 1)) // div down
             .unwrap()
@@ -124,22 +73,22 @@ pub fn log2_x64(mut sqrt_price_x64: u128) -> (bool, u128) {
             .unwrap();
     }
 
-    let msb = msb_x64(sqrt_price_x64.checked_div(scale).unwrap());
-    let mut result = msb.checked_mul(scale).unwrap();
+    let msb = log2_floor_x64(sqrt_price_x64.checked_div(LOG_ONE).unwrap());
+    let mut result = msb.checked_mul(LOG_ONE).unwrap();
     let mut y: U256 = U256::from(sqrt_price_x64) >> msb;
 
-    if y == U256::from(scale) {
+    if y == U256::from(LOG_ONE) {
         return (sign, result);
     };
-    let mut delta = half_scale;
+    let mut delta = LOG_HALF;
     while delta > 0 {
         y = y
             .checked_mul(y)
             .unwrap()
-            .checked_div(U256::from(scale))
+            .checked_div(U256::from(LOG_ONE))
             .unwrap();
 
-        if y >= U256::from(scale_2x) {
+        if y >= U256::from(LOG_DOUBLE) {
             result += delta;
             y >>= 1;
         }
@@ -203,41 +152,6 @@ pub fn get_tick_at_sqrt_price_x64(sqrt_price_x64: u128, sqrt_price_decimal: Deci
 mod tests {
     use super::*;
     use crate::{decimal::Decimal, structs::MAX_TICK};
-
-    #[test]
-    fn test_decimal_to_binary() {
-        // max_sqrt_price = ceil(sqrt(2^64 - 1))
-        // min_sqrt_price = floor(sqrt_price(1/(2^64-1)))
-        let max_sqrt_price = Decimal::new(4294967295999999999884);
-        let min_sqrt_price = Decimal::new(232);
-
-        // base: 2^128
-        {
-            let max_sqrt_price_x128 = decimal_to_x128(max_sqrt_price);
-            let min_sqrt_price_x128 = decimal_to_x128(min_sqrt_price);
-
-            let expected_max_sqrt_price_x128 =
-                U256::from_dec_str("1461501637330902918164212078153454157894181088513").unwrap();
-            let expected_min_sqrt_price_x128 =
-                U256::from_dec_str("78945509125657723523502908924").unwrap();
-
-            assert!(max_sqrt_price_x128.eq(&expected_max_sqrt_price_x128));
-            assert!(min_sqrt_price_x128.eq(&expected_min_sqrt_price_x128));
-        }
-        // base: 2^96
-        {
-            let max_sqrt_price_x96 = decimal_to_x128(max_sqrt_price) >> 32;
-            let min_sqrt_price_x96 = decimal_to_x128(min_sqrt_price) >> 32;
-
-            let expected_max_sqrt_price_x96 =
-                U256::from_dec_str("340282366920938463454184140580113548295").unwrap();
-
-            let expected_min_sqrt_price_x96 = U256::from_dec_str("18380933703309326321").unwrap();
-
-            assert!(max_sqrt_price_x96.eq(&expected_max_sqrt_price_x96));
-            assert!(min_sqrt_price_x96.eq(&expected_min_sqrt_price_x96));
-        }
-    }
 
     #[test]
     fn test_log2_x64() {
