563f1c8aff6dd23189b4d043f87c022aa03b50a4
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-01-21 16:18:59 +0100

    Add scripts

---
+++ scripts/ledger/check.ts
@@ -0,0 +1,23 @@
+import { Market, Network, Pair } from '@invariant-labs/sdk/src'
+import { MOCK_TOKENS } from '@invariant-labs/sdk/src/network'
+import { FEE_TIERS } from '@invariant-labs/sdk/src/utils'
+import { Provider } from '@project-serum/anchor'
+import { clusterApiUrl, PublicKey } from '@solana/web3.js'
+// trunk-ignore(eslint/@typescript-eslint/no-var-requires)
+require('dotenv').config()
+
+const provider = Provider.local(clusterApiUrl('devnet'), {
+  skipPreflight: true
+})
+
+const connection = provider.connection
+
+const main = async () => {
+  const market = await Market.build(Network.DEV, provider.wallet, connection)
+  const state = await market.getState()
+
+  console.log('admin of the state')
+  console.log(`state = ${state.admin.toString()}`)
+}
+// trunk-ignore(eslint/@typescript-eslint/no-floating-promises)
+main()
