a7a7c89786126e1b58bb7fd4bb418a691b5c0454
K-Pomian <pomian.konrad@gmail.com>
2022-01-24 12:21:11 +0100

    Error codes updated

--- programs/invariant/src/errors.rs
+++ programs/invariant/src/errors.rs
@@ -3,45 +3,45 @@ use anchor_lang::prelude::*;
 #[error]
 pub enum ErrorCode {
     #[msg("Amount is zero")]
-    ZeroAmount = 0, // 12c
+    ZeroAmount = 0, // 1770
     #[msg("Output would be zero")]
-    ZeroOutput = 1, // 12d
+    ZeroOutput = 1, // 1771
     #[msg("Not the expected tick")]
-    WrongTick = 2, // 12e
+    WrongTick = 2, // 1772
     #[msg("Price limit is on the wrong side of price")]
-    WrongLimit = 3, // 12f
+    WrongLimit = 3, // 1773
     #[msg("Tick index not divisible by spacing or over limit")]
-    InvalidTickIndex = 4, // 130
+    InvalidTickIndex = 4, // 1774
     #[msg("Invalid tick_lower or tick_upper")]
-    InvalidTickInterval = 5, // 131
+    InvalidTickInterval = 5, // 1775
     #[msg("There is no more tick in that direction")]
-    NoMoreTicks = 6, // 132
+    NoMoreTicks = 6, // 1776
     #[msg("Correct tick not found in context")]
-    TickNotFound = 7, // 133
+    TickNotFound = 7, // 1777
     #[msg("Price would cross swap limit")]
-    PriceLimitReached = 8, // 134
+    PriceLimitReached = 8, // 1778
     #[msg("Invalid tick liquidity")]
-    InvalidTickLiquidity = 9, // 135
+    InvalidTickLiquidity = 9, // 1779
     #[msg("Disable empty position pokes")]
-    EmptyPositionPokes = 10, // 136
+    EmptyPositionPokes = 10, // 177a
     #[msg("Invalid tick liquidity")]
-    InvalidPositionLiquidity = 11, // 137
+    InvalidPositionLiquidity = 11, // 177b
     #[msg("Invalid pool liquidity")]
-    InvalidPoolLiquidity = 12, // 138
+    InvalidPoolLiquidity = 12, // 177c
     #[msg("Invalid position index")]
-    InvalidPositionIndex = 13, // 139
+    InvalidPositionIndex = 13, // 177d
     #[msg("Position liquidity would be zero")]
-    PositionWithoutLiquidity = 14, // 13a
+    PositionWithoutLiquidity = 14, // 177e
     #[msg("You are not admin")]
-    Unauthorized = 15, // 13b
+    Unauthorized = 15, // 177f
     #[msg("Invalid pool token addresses")]
-    InvalidPoolTokenAddresses = 16, // 13c
+    InvalidPoolTokenAddresses = 16, // 1780
     #[msg("Time cannot be negative")]
-    NegativeTime = 17, // 13d
+    NegativeTime = 17, // 1781
     #[msg("Oracle is already initialized")]
-    OracleAlreadyInitialized = 18, // 13e
+    OracleAlreadyInitialized = 18, // 1782
     #[msg("Absolute price limit was reached")]
-    LimitReached = 19, // 13f
+    LimitReached = 19, // 1783
     #[msg("Invalid protocol fee")]
-    InvalidProtocolFee = 20,
+    InvalidProtocolFee = 20, // 1784
 }
