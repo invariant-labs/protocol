435011dd5402e33e9334099e8e2225435fd77119
K-Pomian <pomian.konrad@gmail.com>
2022-02-22 12:44:25 +0100

    Conflicts

--- sdk/src/market.ts
+++ sdk/src/market.ts
@@ -1235,6 +1235,7 @@ export class Market {
 
     await signAndSend(tx, [signer], this.connection)
   }
+<<<<<<< Updated upstream
 
   async getWholeLiquidity(pair: Pair) {
     const poolPublicKey = await pair.getAddress(this.program.programId)
@@ -1275,6 +1276,8 @@ export class Market {
 
     return { volumeX, volumeY }
   }
+=======
+>>>>>>> Stashed changes
 }
 
 export interface Decimal {
