aa84e709f7c2977f8f0edc15bec68903a22772f1
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-03-03 22:18:55 +0100

    Add findTickmapChanges SDK util function

--- sdk/src/tickmap.ts
+++ sdk/src/tickmap.ts
@@ -2,6 +2,10 @@ import { BN } from '@project-serum/anchor'
 import { MAX_TICK, TICK_LIMIT, TICK_SEARCH_RANGE } from '.'
 import { Tickmap, TickPosition } from './market'
 
+export interface TickmapChange {
+  [index: number]: 'added' | 'removed'
+}
+
 export const getSearchLimit = (currentTickIndex: BN, tickSpacing: BN, up: boolean): BN => {
   const index = currentTickIndex.div(tickSpacing)
   let limit: BN = new BN(0)
@@ -101,4 +105,33 @@ export const tickToPosition = (tick: BN, tickSpacing: BN): TickPosition => {
   const bit = Math.abs(bitmapIndex.modn(8))
 
   return { byte, bit }
+}
+
+export const findTickmapChanges = (
+  currentTickmap: number[],
+  nextTickmap: number[],
+  offset: number = -TICK_LIMIT
+): TickmapChange => {
+  if (currentTickmap.length !== nextTickmap.length) {
+    throw new Error('bitmap length mismatch')
+  }
+  let tickmapChanges: TickmapChange = {}
+
+  currentTickmap.forEach((currentByte, i) => {
+    const nextByte = nextTickmap[i]
+    if (currentByte !== nextByte) {
+      const xor = currentByte ^ nextByte
+      for (let bit = 0; bit < 8; bit++) {
+        if ((xor & (1 << bit)) !== 0) {
+          const added = (nextByte & (1 << bit)) !== 0
+          tickmapChanges = {
+            ...tickmapChanges,
+            [i * 8 + bit + offset]: added ? 'added' : 'removed'
+          }
+        }
+      }
+    }
+  })
+
+  return tickmapChanges
 }
