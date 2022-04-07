86fc79871ba045b90ff6381711a47339e199dd84
Robert Godula <robertgodula1993@gmail.com>
2022-04-07 16:46:07 +0200

    remove stake

--- scripts/staker/remove-stake.ts
+++ scripts/staker/remove-stake.ts
@@ -1,26 +1,34 @@
-import { Staker, Network } from '../../sdk-staker/src'
-import { Provider } from '@project-serum/anchor'
+import { Staker, Network } from '../../staker-sdk/src'
+import { Provider, Wallet } from '@project-serum/anchor'
 import { clusterApiUrl, PublicKey } from '@solana/web3.js'
 import { Market } from '@invariant-labs/sdk/lib/market'
+import { MINTER } from '../minter'
+import { FEE_TIERS } from '@invariant-labs/sdk/src/utils'
+import { getMarketAddress, MOCK_TOKENS, Pair } from '@invariant-labs/sdk'
 
 const provider = Provider.local(clusterApiUrl('devnet'), {
   skipPreflight: true
 })
 const connection = provider.connection
+const wallet = new Wallet(MINTER)
 
 // DEFINE ALL THESE VARS BEFORE EXECUTION
-const POOL: PublicKey = new PublicKey('0')
-const FOUNDER: PublicKey = new PublicKey('0')
-const INCENTIVE: PublicKey = new PublicKey('0')
-const POSITION: PublicKey = new PublicKey('0')
+const TOKEN_X: PublicKey = new PublicKey(MOCK_TOKENS.USDC)
+const TOKEN_Y: PublicKey = new PublicKey(MOCK_TOKENS.SOL)
+const OWNER: PublicKey = MINTER.publicKey
+const FOUNDER: PublicKey = MINTER.publicKey
+const INCENTIVE: PublicKey = new PublicKey('9X2p99zymwWpuJb7giF5rmbBLJAv5eDNA2zorpFEyJ4G')
 const POSITION_INDEX = 0
 
 const main = async () => {
-  const staker = await Staker.build(Network.DEV, provider.wallet, connection)
-  const market = await Market.build(Network.DEV, provider.wallet, connection)
-  const position = await market.getPosition(POSITION, POSITION_INDEX)
+  const staker = await Staker.build(Network.DEV, wallet, connection)
+  const market = await Market.build(Network.DEV, wallet, connection)
+  const feeTier = FEE_TIERS[0]
+  const pair = new Pair(TOKEN_X, TOKEN_Y, feeTier)
+  const [poolAddress] = await pair.getAddressAndBump(new PublicKey(getMarketAddress(Network.DEV)))
+  const position = await market.getPosition(OWNER, POSITION_INDEX)
 
-  await staker.removeStake(POOL, position.id, INCENTIVE, FOUNDER)
+  await staker.removeStake(poolAddress, position.id, INCENTIVE, FOUNDER)
 }
 
 // trunk-ignore(eslint/@typescript-eslint/no-floating-promises)
