e8b2bf4be0f295b62f513346386b2a834fdb5710
Robert Godula <robertgodula1993@gmail.com>
2022-06-15 15:30:34 +0200

    get token data fn

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -35,10 +35,12 @@ import {
   priceToTick,
   sqrt
 } from './math'
+import axios, { AxiosResponse } from 'axios'
 import { alignTickToSpacing, getTickFromPrice } from './tick'
 import { getNextTick, getPreviousTick, getSearchLimit } from './tickmap'
 import { struct, u32, u8 } from '@solana/buffer-layout'
 import { u64 } from '@solana/spl-token'
+import { TokenListContainer, TokenListProvider } from '@solana/spl-token-registry'
 
 export const SEED = 'Invariant'
 export const DECIMAL = 12
@@ -993,7 +995,65 @@ export const rewardsAPY = (params: ApyRewardsParams) => {
 
   return { reward, rewardFactor }
 }
+const coingeckoIdOverwrites = {
+  '9vMJfxuKxXBoEa7rM12mYLMwTacLMLDJqHozw96WQL8i': 'terrausd',
+  '7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj': 'lido-staked-sol',
+  NRVwhjBQiUPYtfDT5zRBVJajzFQHaBUNtC7SNVvqRFa: 'nirvana-nirv'
+}
 
+export const getTokensData = async (): Promise<Record<string, TokenData>> => {
+  const tokens: TokenListContainer = await new TokenListProvider().resolve()
+
+  const tokenList = tokens
+    .filterByClusterSlug('mainnet-beta')
+    .getList()
+    .filter(token => token.chainId === 101)
+
+  const tokensObj: Record<string, TokenData> = {}
+
+  tokenList.forEach(token => {
+    tokensObj[token.address.toString()] = {
+      // @ts-ignore
+      id: coingeckoIdOverwrites?.[token.address.toString()] ?? token.extensions?.coingeckoId,
+      decimals: token.decimals,
+      ticker: token.symbol
+    }
+  })
+
+  return tokensObj
+}
+
+export const getCoingeckoTokenPrice = async (id: string): Promise<number> => {
+  return await axios
+    .get<CoingeckoApiPriceData[]>(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}`
+    )
+    .then(res => {
+      return res.data[0].current_price
+    })
+}
+
+// export const getTokensPrices = async (idsList: string[]): Promise<Record<string, number>> => {
+//   const prices = await getCoingeckoPricesData(idsList)
+
+//   const snaps = {}
+
+//   prices.forEach(({ id, currentPrice }) => {
+//     snaps[id] = currentPrice ?? 0
+//   })
+
+//   return snaps
+// }
+export interface CoingeckoApiPriceData {
+  id: string
+  current_price: number
+}
+
+export interface TokenData {
+  id: string
+  decimals: number
+  ticker: string
+}
 export interface ParsedTick {
   liquidity: BN
   index: number
