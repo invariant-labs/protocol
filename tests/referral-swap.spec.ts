576f62998f67fb6a4e7bb2b4626881c15156a74e
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2022-07-01 11:25:08 +0200

    Add referral test

---
+++ tests/referral-swap.spec.ts
@@ -0,0 +1,180 @@
+import * as anchor from '@project-serum/anchor'
+import { Provider, BN } from '@project-serum/anchor'
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { Keypair } from '@solana/web3.js'
+import { assert } from 'chai'
+import { createToken, initEverything } from './testUtils'
+import { Market, Pair, LIQUIDITY_DENOMINATOR, Network } from '@invariant-labs/sdk'
+import { FeeTier } from '@invariant-labs/sdk/lib/market'
+import { fromFee } from '@invariant-labs/sdk/lib/utils'
+import { toDecimal, tou64 } from '@invariant-labs/sdk/src/utils'
+import { CreateTick, InitPosition, Swap } from '@invariant-labs/sdk/src/market'
+
+describe('Referral swap', () => {
+  const provider = Provider.local()
+  const connection = provider.connection
+  // @ts-expect-error
+  const wallet = provider.wallet.payer as Keypair
+  const mintAuthority = Keypair.generate()
+  const admin = Keypair.generate()
+  const feeTier: FeeTier = {
+    fee: fromFee(new BN(500)),
+    tickSpacing: 5
+  }
+  let market: Market
+  let pair: Pair
+  let tokenX: Token
+  let tokenY: Token
+
+  before(async () => {
+    market = await Market.build(
+      Network.LOCAL,
+      provider.wallet,
+      connection,
+      anchor.workspace.Invariant.programId
+    )
+
+    // Request airdrops
+    await Promise.all([
+      connection.requestAirdrop(mintAuthority.publicKey, 1e9),
+      connection.requestAirdrop(admin.publicKey, 1e9)
+    ])
+    // Create tokens
+    const tokens = await Promise.all([
+      createToken(connection, wallet, mintAuthority),
+      createToken(connection, wallet, mintAuthority)
+    ])
+
+    pair = new Pair(tokens[0].publicKey, tokens[1].publicKey, feeTier)
+    tokenX = new Token(connection, pair.tokenX, TOKEN_PROGRAM_ID, wallet)
+    tokenY = new Token(connection, pair.tokenY, TOKEN_PROGRAM_ID, wallet)
+  })
+
+  it('#init()', async () => {
+    await initEverything(market, [pair], admin)
+  })
+
+  it('#swap() crossing tick with referral swap', async () => {
+    // Deposit
+    const upperTick = 10
+    const createTickVars: CreateTick = {
+      pair,
+      index: upperTick,
+      payer: admin.publicKey
+    }
+    await market.createTick(createTickVars, admin)
+
+    const lowerTick = -20
+    const createTickVars2: CreateTick = {
+      pair,
+      index: lowerTick,
+      payer: admin.publicKey
+    }
+    await market.createTick(createTickVars2, admin)
+
+    const positionOwner = Keypair.generate()
+    const referralAccount = Keypair.generate()
+    await connection.requestAirdrop(positionOwner.publicKey, 1e9)
+    const userTokenXAccount = await tokenX.createAccount(positionOwner.publicKey)
+    const userTokenYAccount = await tokenY.createAccount(positionOwner.publicKey)
+    const referralTokenXAccount = await tokenX.createAccount(referralAccount.publicKey)
+    const mintAmount = tou64(new BN(10).pow(new BN(10)))
+
+    await tokenX.mintTo(userTokenXAccount, mintAuthority.publicKey, [mintAuthority], mintAmount)
+    await tokenY.mintTo(userTokenYAccount, mintAuthority.publicKey, [mintAuthority], mintAmount)
+    const liquidityDelta = { v: new BN(1000000).mul(LIQUIDITY_DENOMINATOR) }
+
+    const initPositionVars: InitPosition = {
+      pair,
+      owner: positionOwner.publicKey,
+      userTokenX: userTokenXAccount,
+      userTokenY: userTokenYAccount,
+      lowerTick: -Infinity,
+      upperTick: Infinity,
+      liquidityDelta,
+      knownPrice: (await market.getPool(pair)).sqrtPrice,
+      slippage: { v: new BN(0) }
+    }
+    await market.initPosition(initPositionVars, positionOwner)
+    await market.initPosition(
+      {
+        pair,
+        owner: positionOwner.publicKey,
+        userTokenX: userTokenXAccount,
+        userTokenY: userTokenYAccount,
+        lowerTick: lowerTick,
+        upperTick: upperTick,
+        liquidityDelta,
+        knownPrice: (await market.getPool(pair)).sqrtPrice,
+        slippage: { v: new BN(0) }
+      },
+      positionOwner
+    )
+
+    // Create owner
+    const owner = Keypair.generate()
+    await connection.requestAirdrop(owner.publicKey, 1e9)
+
+    const amount = new BN(100000)
+    const accountX = await tokenX.createAccount(owner.publicKey)
+    const accountY = await tokenY.createAccount(owner.publicKey)
+    await tokenX.mintTo(accountX, mintAuthority.publicKey, [mintAuthority], tou64(amount))
+
+    // Swap
+    const poolDataBefore = await market.getPool(pair)
+    const reserveXBefore = (await tokenX.getAccountInfo(poolDataBefore.tokenXReserve)).amount
+    const reserveYBefore = (await tokenY.getAccountInfo(poolDataBefore.tokenYReserve)).amount
+
+    console.log(`referralTokenXAccount = ${referralTokenXAccount.toString()}`)
+    console.log('Before swap')
+    const referralTokenXBefore = (await tokenX.getAccountInfo(referralTokenXAccount)).amount
+    console.log(referralTokenXBefore.toString())
+
+    const swapVars: Swap = {
+      pair,
+      xToY: true,
+      amount,
+      estimatedPriceAfterSwap: poolDataBefore.sqrtPrice, // ignore price impact using high slippage tolerance
+      slippage: toDecimal(1, 0),
+      accountX,
+      accountY,
+      byAmountIn: true,
+      owner: owner.publicKey,
+      referralAccount: referralTokenXAccount
+    }
+    await market.swap(swapVars, owner)
+
+    console.log('After swap')
+    const referralTokenXAfter = (await tokenX.getAccountInfo(referralTokenXAccount)).amount
+    console.log(referralTokenXAfter.toString())
+
+    // Check pool
+    const poolData = await market.getPool(pair)
+    assert.isFalse(poolData.liquidity.v.eq(poolDataBefore.liquidity.v))
+    assert.equal(poolData.currentTickIndex, -1880)
+    assert.ok(poolData.sqrtPrice.v.lt(poolDataBefore.sqrtPrice.v))
+
+    // Check amounts and fees
+    // const amountX = (await tokenX.getAccountInfo(accountX)).amount
+    // const amountY = (await tokenY.getAccountInfo(accountY)).amount
+    // const reserveXAfter = (await tokenX.getAccountInfo(poolData.tokenXReserve)).amount
+    // const reserveYAfter = (await tokenY.getAccountInfo(poolData.tokenYReserve)).amount
+    // const reserveXDelta = reserveXAfter.sub(reserveXBefore)
+    // const reserveYDelta = reserveYBefore.sub(reserveYAfter)
+
+    // CALCULATING STATS REQUIRE SAME METHOD TO SWAP WITH REFERRAL AND WITHOUT REFERRAL
+    // fee tokens           0.005 * 100000 = 500
+    // protocol fee tokens  ceil(500 * 0.01) = 5
+    // referral fee
+    // pool fee tokens      500 - 5 = 5
+    // fee growth global    5/1000000 = 5 * 10^-6
+    // assert.ok(amountX.eqn(0))
+    // assert.ok(amountY.eq(amount.subn(7)))
+    // assert.ok(reserveXDelta.eq(amount))
+    // assert.ok(reserveYDelta.eq(amount.subn(7)))
+    // assert.equal(poolData.feeGrowthGlobalX.v.toString(), '5000000000000000000')
+    // assert.ok(poolData.feeGrowthGlobalY.v.eqn(0))
+    // assert.ok(poolData.feeProtocolTokenX.eqn(1))
+    // assert.ok(poolData.feeProtocolTokenY.eqn(0))
+  })
+})
