1dccc3551809bb34a472ee947a9f71fb38002229
Beata Korzeniewska <b.a.korzeniewska@wp.pl>
2022-07-29 11:40:30 +0200

    fixed warning with cfg

--- programs/invariant/src/referral.rs
+++ programs/invariant/src/referral.rs
@@ -1,28 +1,29 @@
 pub mod referral {
     use anchor_lang::prelude::Pubkey;
-    use std::{borrow::Borrow, str::FromStr};
-    pub fn contains(ref_owner: Pubkey) -> bool {
+    #[cfg(feature = "jupiter")]
+    use std::str::FromStr;
+
+    pub fn contains(_ref_owner: Pubkey) -> bool {
         #[cfg(feature = "jupiter")]
         {
             let jup3 = Pubkey::from_str(&"JUP3c2Uh3WA4Ng34tw6kPd2G4C5BB21Xo36Je1s32Ph").unwrap();
             let jup2 = Pubkey::from_str(&"JUP2jxvXaqu7NQY1GmNF4m1vodw12LVXYxbFL2uJvfo").unwrap();
-            if ref_owner.eq(&jup3) || ref_owner.eq(&jup2) {
+            if _ref_owner.eq(&jup3) || _ref_owner.eq(&jup2) {
                 return true;
             }
         }
 
         #[cfg(feature = "all")]
         return true;
-        #[cfg(feature = "none")]
-        return false;
 
-        false
+        #[cfg(not(feature = "all"))]
+        return false;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{borrow::Borrow, str::FromStr};
+    use std::str::FromStr;
 
     use anchor_lang::prelude::Pubkey;
 
@@ -31,8 +32,8 @@ mod tests {
     #[test]
     #[cfg_attr(not(feature = "all"), ignore)]
     fn test_is_referral_for_all() {
-        let is_constains = contains(Pubkey::default());
-        assert!(is_constains);
+        let is_contains = contains(Pubkey::default());
+        assert!(is_contains);
     }
 
     #[test]
@@ -40,6 +41,8 @@ mod tests {
     fn test_is_referral_for_none() {
         let pubkey = Pubkey::from_str(&"7CKCpJWFRu1WAWfCvDkwyniP6JGpSyMf4Bkxk2U6v2Ej").unwrap();
         assert!(!contains(pubkey));
+        let jup2 = Pubkey::from_str(&"JUP2jxvXaqu7NQY1GmNF4m1vodw12LVXYxbFL2uJvfo").unwrap();
+        assert!(!contains(jup2));
     }
 
     #[test]
