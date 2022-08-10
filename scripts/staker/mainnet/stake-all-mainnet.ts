bd3b8e7ba9af01895f5d1c2ae1d64af91facbc0f
Robert Godula <robertgodula1993@gmail.com>
2022-08-10 15:17:00 +0200

    mainnet scripts update

--- scripts/staker/mainnet/stake-all-mainnet.ts
+++ scripts/staker/mainnet/stake-all-mainnet.ts
@@ -1,18 +1,21 @@
-import { Staker, Network } from '../../staker-sdk/src'
-import { Provider, utils, Wallet } from '@project-serum/anchor'
-import { clusterApiUrl, Keypair, PublicKey } from '@solana/web3.js'
-import { Market, UpdateSecondsPerLiquidity } from '@invariant-labs/sdk/src/market'
-import { CreateStake } from '../../staker-sdk/lib/staker'
-import { getMarketAddress, Pair, MOCK_TOKENS } from '@invariant-labs/sdk/src'
+import { Staker, Network } from '../../../staker-sdk/src'
+import { Provider, Wallet } from '@project-serum/anchor'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import { Market, UpdateSecondsPerLiquidity } from '@invariant-labs/sdk/lib/market'
+import { CreateStake } from '../../../staker-sdk/lib/staker'
+import { getMarketAddress, Pair } from '@invariant-labs/sdk/src'
 import { FEE_TIERS, getPositionIndex } from '@invariant-labs/sdk/src/utils'
 import { MAINNET_TOKENS } from '@invariant-labs/sdk/lib/network'
 
 // trunk-ignore(eslint/@typescript-eslint/no-var-requires)
 require('dotenv').config()
 
-const provider = Provider.local('https://api.mainnet-beta.solana.com', {
-  skipPreflight: true
-})
+const provider = Provider.local(
+  'https://tame-ancient-mountain.solana-mainnet.quiknode.pro/6a9a95bf7bbb108aea620e7ee4c1fd5e1b67cc62/',
+  {
+    skipPreflight: true
+  }
+)
 
 const connection = provider.connection
 // @ts-expect-error
@@ -20,7 +23,7 @@ const wallet = provider.wallet.payer as Keypair
 const signer = new Wallet(wallet)
 
 // DEFINE ALL THESE VARS BEFORE EXECUTION
-const INCENTIVE: PublicKey = new PublicKey('EfAJjXZppDeQPRpfjkndsPjUrp6hjcMuACHhghNmYE1t')
+const INCENTIVE: PublicKey = new PublicKey('HvVL7Ny8f4nF9xH6BTq7LtbjEiNgw92YhC77QDP2yjyn')
 const TOKEN_USDC: PublicKey = new PublicKey(MAINNET_TOKENS.USDC)
 const TOKEN_USDT: PublicKey = new PublicKey(MAINNET_TOKENS.USDH)
 const INVARIANT = new PublicKey(getMarketAddress(Network.MAIN))
@@ -32,11 +35,10 @@ const main = async () => {
 
   const pair = new Pair(TOKEN_USDC, TOKEN_USDT, FEE_TIER)
   const [poolAddress] = await pair.getAddressAndBump(INVARIANT)
-  const pool = await market.getPool(pair)
   const positions = await market.getPositionsForPool(poolAddress)
 
   for (const position of positions) {
-    const index = getPositionIndex(position.address, INVARIANT, position.owner)
+    const index = await getPositionIndex(position.address, INVARIANT, position.owner)
     const update: UpdateSecondsPerLiquidity = {
       pair,
       owner: position.owner,
@@ -57,7 +59,7 @@ const main = async () => {
     }
 
     const result = await staker.createStake(market, update, createStake)
-    console.log(`Created stake `, result.stringTx)
+    console.log('Created stake', result.stringTx)
   }
 }
 
