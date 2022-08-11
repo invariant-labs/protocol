069373d3da6d111b329f695ee53dc56b74ea2090
Robert Godula <robertgodula1993@gmail.com>
2022-08-11 16:51:21 +0200

    max cross draft

---
+++ tests/max-tick-cross.spec.ts
@@ -0,0 +1,129 @@
+import * as anchor from '@project-serum/anchor'
+import { Provider, BN } from '@project-serum/anchor'
+import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { Keypair } from '@solana/web3.js'
+import { assert } from 'chai'
+import { createToken, initEverything } from './testUtils'
+import { Market, Pair, LIQUIDITY_DENOMINATOR, Network } from '@invariant-labs/sdk/src'
+import { FeeTier } from '@invariant-labs/sdk/lib/market'
+import { fromFee } from '@invariant-labs/sdk/lib/utils'
+import { toDecimal, tou64 } from '@invariant-labs/sdk/src/utils'
+import { CreateTick, InitPosition, Swap } from '@invariant-labs/sdk/src/market'
+import { sleep } from '@invariant-labs/sdk'
+
+describe('swap', () => {
+  //const provider = Provider.local(undefined, { skipPreflight: true })
+  const provider = Provider.local()
+  const connection = provider.connection
+  // @ts-expect-error
+  const wallet = provider.wallet.payer as Keypair
+  const mintAuthority = Keypair.generate()
+  const admin = Keypair.generate()
+  const feeTier: FeeTier = {
+    fee: fromFee(new BN(600)),
+    tickSpacing: 10
+  }
+  let market: Market
+  let pair: Pair
+  let tokenX: Token
+  let tokenY: Token
+
+  before(async () => {
+    market = await Market.build(
+      Network.LOCAL,
+      provider.wallet,
+      connection,
+      anchor.workspace.Invariant.programId
+    )
+
+    // Request airdrops
+    await Promise.all([
+      connection.requestAirdrop(mintAuthority.publicKey, 1e9),
+      connection.requestAirdrop(admin.publicKey, 1e9)
+    ])
+    // Create tokens
+    const tokens = await Promise.all([
+      createToken(connection, wallet, mintAuthority),
+      createToken(connection, wallet, mintAuthority)
+    ])
+
+    pair = new Pair(tokens[0].publicKey, tokens[1].publicKey, feeTier)
+    tokenX = new Token(connection, pair.tokenX, TOKEN_PROGRAM_ID, wallet)
+    tokenY = new Token(connection, pair.tokenY, TOKEN_PROGRAM_ID, wallet)
+  })
+
+  it('#init()', async () => {
+    await initEverything(market, [pair], admin)
+  })
+
+  it('#swap() within a tick', async () => {
+    // Deposit
+    for (let index = -200; index < 50; index += 10) {
+      const createTickVars: CreateTick = {
+        pair,
+        index,
+        payer: admin.publicKey
+      }
+      await market.createTick(createTickVars, admin)
+    }
+
+    const positionOwner = Keypair.generate()
+    await connection.requestAirdrop(positionOwner.publicKey, 1e9)
+    const userTokenXAccount = await tokenX.createAccount(positionOwner.publicKey)
+    const userTokenYAccount = await tokenY.createAccount(positionOwner.publicKey)
+    const mintAmount = tou64(new BN(10).pow(new BN(10)))
+
+    await tokenX.mintTo(userTokenXAccount, mintAuthority.publicKey, [mintAuthority], mintAmount)
+    await tokenY.mintTo(userTokenYAccount, mintAuthority.publicKey, [mintAuthority], mintAmount)
+    const liquidityDelta = { v: new BN(1000000).mul(LIQUIDITY_DENOMINATOR) }
+
+    await market.createPositionList(positionOwner.publicKey, positionOwner)
+
+    for (let i = -200; i < 50; i += 10) {
+      const initPositionVars: InitPosition = {
+        pair,
+        owner: positionOwner.publicKey,
+        userTokenX: userTokenXAccount,
+        userTokenY: userTokenYAccount,
+        lowerTick: i,
+        upperTick: i + 10,
+        liquidityDelta,
+        knownPrice: (await market.getPool(pair)).sqrtPrice,
+        slippage: { v: new BN(0) }
+      }
+      await market.initPosition(initPositionVars, positionOwner)
+    }
+
+    assert.ok((await market.getPool(pair)).liquidity.v.eq(liquidityDelta.v))
+
+    // Create owner
+    const owner = Keypair.generate()
+    await connection.requestAirdrop(owner.publicKey, 1e9)
+
+    const amount = new BN(3000)
+    const accountX = await tokenX.createAccount(owner.publicKey)
+    const accountY = await tokenY.createAccount(owner.publicKey)
+    await tokenX.mintTo(accountX, mintAuthority.publicKey, [mintAuthority], tou64(amount))
+
+    // Swap
+    const poolDataBefore = await market.getPool(pair)
+
+    const swapVars: Swap = {
+      pair,
+      xToY: true,
+      amount,
+      estimatedPriceAfterSwap: poolDataBefore.sqrtPrice, // ignore price impact using high slippage tolerance
+      slippage: toDecimal(2, 2),
+      accountX,
+      accountY,
+      byAmountIn: true,
+      owner: owner.publicKey
+    }
+    await market.swap(swapVars, owner)
+
+    // // Check pool
+    const poolData = await market.getPool(pair)
+    console.log(poolData.currentTickIndex)
+    // assert.equal(poolData.currentTickIndex, -20)
+  })
+})
