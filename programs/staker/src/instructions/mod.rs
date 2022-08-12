51cafbdb86cba3b719a15dbe9a231a1964caec1e
Robert Godula <robertgodula1993@gmail.com>
2022-08-12 16:18:17 +0200

    add close_stake_by_owner instruction

--- programs/staker/src/instructions/mod.rs
+++ programs/staker/src/instructions/mod.rs
@@ -1,9 +1,11 @@
+pub mod close_stake_by_owner;
 pub mod create_incentive;
 pub mod end_incentive;
 pub mod remove_stake;
 pub mod stake;
 pub mod withdraw;
 
+pub use close_stake_by_owner::*;
 pub use create_incentive::*;
 pub use end_incentive::*;
 pub use remove_stake::*;
