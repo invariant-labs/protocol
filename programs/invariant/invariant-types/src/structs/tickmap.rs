3fd88728ce59ba891f258fc98e8e15351eff7d4e
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-05 21:58:52 +0100

    update Tickmap invariant-types

--- programs/invariant/invariant-types/src/structs/tickmap.rs
+++ programs/invariant/invariant-types/src/structs/tickmap.rs
@@ -1,14 +1,17 @@
 use crate::size;
 use anchor_lang::prelude::*;
-// use borsh::BorshDeserialize;
 
-// #[derive(Default, Debug, BorshDeserialize, AnchorDeserialize)]
-// #[derive(Default, Debug, BorshSerialize, BorshDeserialize)]
 #[account(zero_copy)]
 #[repr(packed)]
 #[derive(AnchorDeserialize)]
 pub struct Tickmap {
     pub bitmap: [u8; 11091], // Tick limit / 4
+}
+
+impl Default for Tickmap {
+    fn default() -> Self {
+        Tickmap { bitmap: [0; 11091] }
+    }
 }
 
 size!(Tickmap);
