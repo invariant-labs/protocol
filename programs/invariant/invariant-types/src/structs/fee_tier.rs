ddb3de0d51fb5f06be707250e20b9b5d8794b0e8
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-05 18:22:09 +0100

    test

--- programs/invariant/invariant-types/src/structs/fee_tier.rs
+++ programs/invariant/invariant-types/src/structs/fee_tier.rs
@@ -1,11 +1,9 @@
 use crate::{decimals::FixedPoint, size};
 use anchor_lang::prelude::*;
-use borsh::BorshDeserialize;
 
 #[account(zero_copy)]
-#[repr(packed)]
-#[repr(C)]
-#[derive(PartialEq, Default, Debug, BorshDeserialize)]
+// #[repr(packed)]
+#[derive(PartialEq, Default, Debug, AnchorDeserialize, AnchorSerialize)]
 pub struct FeeTier {
     pub fee: FixedPoint,
     pub tick_spacing: u16,
