10b26d4613522ec19d05d0ab3db607f7dc25f9f3
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-12 14:18:39 +0100

    Add compute_swap_step calculation to invariant-types

--- programs/invariant/invariant-types/src/math.rs
+++ programs/invariant/invariant-types/src/math.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use anchor_lang::*;
 
 use crate::{
@@ -5,6 +7,14 @@ use crate::{
     errors::InvariantErrorCode,
     structs::{get_search_limit, Tickmap, MAX_TICK},
 };
+
+#[derive(PartialEq, Debug)]
+pub struct SwapResult {
+    pub next_price_sqrt: Price,
+    pub amount_in: TokenAmount,
+    pub amount_out: TokenAmount,
+    pub fee_amount: TokenAmount,
+}
 
 // converts ticks to price with reduced precision
 pub fn calculate_price_sqrt(tick_index: i32) -> Price {
@@ -119,5 +129,242 @@ pub fn get_closer_limit(
                 Ok((sqrt_price_limit, None))
             }
         }
+    }
+}
+
+pub fn compute_swap_step(
+    current_price_sqrt: Price,
+    target_price_sqrt: Price,
+    liquidity: Liquidity,
+    amount: TokenAmount,
+    by_amount_in: bool,
+    fee: FixedPoint,
+) -> SwapResult {
+    if liquidity.is_zero() {
+        return SwapResult {
+            next_price_sqrt: target_price_sqrt,
+            amount_in: TokenAmount(0),
+            amount_out: TokenAmount(0),
+            fee_amount: TokenAmount(0),
+        };
+    }
+
+    let x_to_y = current_price_sqrt >= target_price_sqrt;
+
+    let next_price_sqrt;
+    let mut amount_in = TokenAmount(0);
+    let mut amount_out = TokenAmount(0);
+
+    if by_amount_in {
+        let amount_after_fee = amount.big_mul(FixedPoint::from_integer(1u8) - fee);
+
+        amount_in = if x_to_y {
+            get_delta_x(target_price_sqrt, current_price_sqrt, liquidity, true)
+        } else {
+            get_delta_y(current_price_sqrt, target_price_sqrt, liquidity, true)
+        }
+        .unwrap_or(TokenAmount(u64::MAX));
+
+        // if target price was hit it will be the next price
+        if amount_after_fee >= amount_in {
+            next_price_sqrt = target_price_sqrt
+        } else {
+            next_price_sqrt = get_next_sqrt_price_from_input(
+                current_price_sqrt,
+                liquidity,
+                amount_after_fee,
+                x_to_y,
+            )
+        };
+    } else {
+        amount_out = if x_to_y {
+            get_delta_y(target_price_sqrt, current_price_sqrt, liquidity, false)
+        } else {
+            get_delta_x(current_price_sqrt, target_price_sqrt, liquidity, false)
+        }
+        .unwrap_or(TokenAmount(u64::MAX));
+
+        if amount >= amount_out {
+            next_price_sqrt = target_price_sqrt
+        } else {
+            next_price_sqrt =
+                get_next_sqrt_price_from_output(current_price_sqrt, liquidity, amount, x_to_y)
+        }
+    }
+
+    let not_max = target_price_sqrt != next_price_sqrt;
+
+    if x_to_y {
+        if not_max || !by_amount_in {
+            amount_in = get_delta_x(next_price_sqrt, current_price_sqrt, liquidity, true).unwrap()
+        };
+        if not_max || by_amount_in {
+            amount_out = get_delta_y(next_price_sqrt, current_price_sqrt, liquidity, false).unwrap()
+        }
+    } else {
+        if not_max || !by_amount_in {
+            amount_in = get_delta_y(current_price_sqrt, next_price_sqrt, liquidity, true).unwrap()
+        };
+        if not_max || by_amount_in {
+            amount_out = get_delta_x(current_price_sqrt, next_price_sqrt, liquidity, false).unwrap()
+        }
+    }
+
+    // Amount out can not exceed amount
+    if !by_amount_in && amount_out > amount {
+        amount_out = amount;
+    }
+
+    let fee_amount = if by_amount_in && next_price_sqrt != target_price_sqrt {
+        amount - amount_in
+    } else {
+        amount_in.big_mul_up(fee)
+    };
+
+    SwapResult {
+        next_price_sqrt,
+        amount_in,
+        amount_out,
+        fee_amount,
+    }
+}
+
+// delta x = (L * delta_sqrt_price) / (lower_sqrt_price * higher_sqrt_price)
+pub fn get_delta_x(
+    sqrt_price_a: Price,
+    sqrt_price_b: Price,
+    liquidity: Liquidity,
+    up: bool,
+) -> Option<TokenAmount> {
+    let delta_price = if sqrt_price_a > sqrt_price_b {
+        sqrt_price_a - sqrt_price_b
+    } else {
+        sqrt_price_b - sqrt_price_a
+    };
+
+    // log(2,  2^16 * 10^24 * 2^128 / 10^6 ) = 203.7
+    let nominator = delta_price.big_mul_to_value(liquidity);
+    match up {
+        true => Price::big_div_values_to_token_up(
+            nominator,
+            sqrt_price_a.big_mul_to_value(sqrt_price_b),
+        ),
+        false => Price::big_div_values_to_token(
+            nominator,
+            sqrt_price_a.big_mul_to_value_up(sqrt_price_b),
+        ),
+    }
+}
+
+// delta y = L * delta_sqrt_price
+pub fn get_delta_y(
+    sqrt_price_a: Price,
+    sqrt_price_b: Price,
+    liquidity: Liquidity,
+    up: bool,
+) -> Option<TokenAmount> {
+    let delta_price = if sqrt_price_a > sqrt_price_b {
+        sqrt_price_a - sqrt_price_b
+    } else {
+        sqrt_price_b - sqrt_price_a
+    };
+
+    match match up {
+        true => delta_price
+            .big_mul_to_value_up(liquidity)
+            .checked_add(Price::almost_one())
+            .unwrap()
+            .checked_div(Price::one())
+            .unwrap()
+            .try_into(),
+        false => delta_price
+            .big_mul_to_value(liquidity)
+            .checked_div(Price::one())
+            .unwrap()
+            .try_into(),
+    } {
+        Ok(x) => Some(TokenAmount(x)),
+        Err(_) => None,
+    }
+}
+
+fn get_next_sqrt_price_from_input(
+    price_sqrt: Price,
+    liquidity: Liquidity,
+    amount: TokenAmount,
+    x_to_y: bool,
+) -> Price {
+    assert!(!price_sqrt.is_zero());
+    assert!(!liquidity.is_zero());
+
+    if x_to_y {
+        get_next_sqrt_price_x_up(price_sqrt, liquidity, amount, true)
+    } else {
+        get_next_sqrt_price_y_down(price_sqrt, liquidity, amount, true)
+    }
+}
+
+fn get_next_sqrt_price_from_output(
+    price_sqrt: Price,
+    liquidity: Liquidity,
+    amount: TokenAmount,
+    x_to_y: bool,
+) -> Price {
+    assert!(!price_sqrt.is_zero());
+    assert!(!liquidity.is_zero());
+
+    if x_to_y {
+        get_next_sqrt_price_y_down(price_sqrt, liquidity, amount, false)
+    } else {
+        get_next_sqrt_price_x_up(price_sqrt, liquidity, amount, false)
+    }
+}
+
+// L * price / (L +- amount * price)
+fn get_next_sqrt_price_x_up(
+    price_sqrt: Price,
+    liquidity: Liquidity,
+    amount: TokenAmount,
+    add: bool,
+) -> Price {
+    if amount.is_zero() {
+        return price_sqrt;
+    };
+
+    let big_liquidity = liquidity
+        .here::<U256>()
+        .checked_mul(U256::from(PRICE_LIQUIDITY_DENOMINATOR))
+        .unwrap();
+
+    let denominator = match add {
+        true => big_liquidity.checked_add(price_sqrt.big_mul_to_value(amount)),
+        false => big_liquidity.checked_sub(price_sqrt.big_mul_to_value(amount)),
+    }
+    .unwrap();
+
+    Price::big_div_values_up(price_sqrt.big_mul_to_value_up(liquidity), denominator)
+}
+
+// price +- (amount / L)
+fn get_next_sqrt_price_y_down(
+    price_sqrt: Price,
+    liquidity: Liquidity,
+    amount: TokenAmount,
+    add: bool,
+) -> Price {
+    if add {
+        let quotient = Price::from_decimal(amount).big_div_by_number(
+            U256::from(liquidity.get())
+                .checked_mul(U256::from(PRICE_LIQUIDITY_DENOMINATOR))
+                .unwrap(),
+        );
+        price_sqrt + quotient
+    } else {
+        let quotient = Price::from_decimal(amount).big_div_by_number_up(
+            U256::from(liquidity.get())
+                .checked_mul(U256::from(PRICE_LIQUIDITY_DENOMINATOR))
+                .unwrap(),
+        );
+        price_sqrt - quotient
     }
 }
