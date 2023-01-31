45860174dca2e11ce16c478ae47c804dd797444e
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2023-01-31 17:26:26 +0100

    Update SDK version  - new fee tiers

--- scripts/ledger/deploy.ts
+++ scripts/ledger/deploy.ts
@@ -1,8 +1,7 @@
 import { Network } from '@invariant-labs/sdk'
 import { FeeTier } from '@invariant-labs/sdk/lib/market'
 import { fromFee } from '@invariant-labs/sdk/lib/utils'
-import { CreateFeeTier, Market } from '@invariant-labs/sdk/src/market'
-import { FEE_TIERS } from '@invariant-labs/sdk/src/utils'
+import { Market } from '@invariant-labs/sdk/src/market'
 import { Provider } from '@project-serum/anchor'
 import { clusterApiUrl, PublicKey } from '@solana/web3.js'
 import { BN } from '../../staker-sdk/lib'
@@ -12,24 +11,27 @@ import { getLedgerWallet, signAndSendLedger } from '../walletProvider/wallet'
 // trunk-ignore(eslint/@typescript-eslint/no-var-requires)
 require('dotenv').config()
 
-const provider = Provider.local(clusterApiUrl('devnet'), {
+const provider = Provider.local('FILL ME', {
   skipPreflight: true
 })
 const connection = provider.connection
 
-const createStandardFeeTiers = async (market: Market, ledgerWallet: LedgerWalletProvider) => {
-  for (const feeTier of FEE_TIERS) {
-    const createFeeTierVars: CreateFeeTier = {
-      feeTier: feeTier,
-      admin: ledgerWallet.publicKey
-    }
-    const createFeeTierTx = await market.createFeeTierTransaction(createFeeTierVars)
-    await signAndSendLedger(createFeeTierTx, connection, ledgerWallet)
-  }
-}
+// const createStandardFeeTiers = async (market: Market, ledgerWallet: LedgerWalletProvider) => {
+//   for (const feeTier of FEE_TIERS) {
+//     const createFeeTierVars: CreateFeeTier = {
+//       feeTier: feeTier,
+//       admin: ledgerWallet.publicKey
+//     }
+//     const createFeeTierTx = await market.createFeeTierTransaction(createFeeTierVars)
+//     await signAndSendLedger(createFeeTierTx, connection, ledgerWallet)
+//   }
+// }
 
-const createLowestFeeTier = async (market: Market, ledgerWallet: LedgerWalletProvider) => {
-  const feeTier: FeeTier = { fee: fromFee(new BN(1)), tickSpacing: 1 }
+const createFeeTier = async (
+  market: Market,
+  feeTier: FeeTier,
+  ledgerWallet: LedgerWalletProvider
+) => {
   const createFeeTierTx = await market.createFeeTierTransaction({
     feeTier: feeTier,
     admin: ledgerWallet.publicKey
@@ -37,15 +39,17 @@ const createLowestFeeTier = async (market: Market, ledgerWallet: LedgerWalletPro
   await signAndSendLedger(createFeeTierTx, connection, ledgerWallet)
 }
 
+// const createLowestFeeTier = async (market: Market, ledgerWallet: LedgerWalletProvider) => {
+//   await createFeeTier(market, { fee: fromFee(new BN(1)), tickSpacing: 1 }, ledgerWallet)
+// }
+
 const main = async () => {
   const ledgerWallet = await getLedgerWallet()
   console.log(`ledger public key: ${ledgerWallet.publicKey}`)
   const market = await Market.build(Network.MAIN, provider.wallet, connection)
-  await createLowestFeeTier(market, ledgerWallet)
-
-  // const createStateTx = await market.createStateTransaction(ledgerWallet.publicKey)
-  // await signAndSendLedger(createStateTx, connection, ledgerWallet)
-  // await createStandardFeeTiers(market, ledgerWallet)
+  await createFeeTier(market, { fee: fromFee(new BN(5000)), tickSpacing: 5 }, ledgerWallet)
+  await createFeeTier(market, { fee: fromFee(new BN(10000)), tickSpacing: 5 }, ledgerWallet)
+  await createFeeTier(market, { fee: fromFee(new BN(50000)), tickSpacing: 5 }, ledgerWallet)
 }
 
 // trunk-ignore(eslint/@typescript-eslint/no-floating-promises)
