3868e63b14663fdd6639db7046870cac5e0986fa
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-27 02:23:41 +0200

    Simulation swap of USDC/FTT 50% fee tier

--- scripts/ftt-usdc-50-percentage.ts
+++ scripts/ftt-usdc-50-percentage.ts
@@ -1,5 +1,6 @@
-import { FeeTier } from '@invariant-labs/sdk/lib/market'
+import { FeeTier, Tick } from '@invariant-labs/sdk/lib/market'
 import { Market, Network, Pair } from '@invariant-labs/sdk/src'
+import { simulateSwap, toDecimal } from '@invariant-labs/sdk/src/utils'
 import { BN, Provider } from '@project-serum/anchor'
 import { PublicKey } from '@solana/web3.js'
 // trunk-ignore(eslint/@typescript-eslint/no-var-requires)
@@ -22,10 +23,55 @@ const main = async () => {
         tickSpacing: 5
     }
     const pair = new Pair(USDC, FTT, feeTier)
+    // UPDATE SWAP DIRECTION HERE
+    const usdcToFtt = false
+    let xToY = usdcToFtt
+    if (pair.tokenX.equals(USDC)) {
+        console.log('x = USDC')
+        console.log('y = FTT')
+    } else {
+        xToY = !usdcToFtt
+        console.log('x = FTT')
+        console.log('y = USDC')
+    }
 
-    const poolData = await market.getPool(pair)
-    console.log(poolData)
+    console.log(`tokenX = ${pair.tokenX.toString()}`)
+    console.log(pair.tokenX)
 
+    // pool data
+    const pool = await market.getPool(pair)
+    console.log(pool)
+
+    // simulate swap
+    const ticks: Map<number, Tick> = new Map(
+        (await market.getAllTicks(pair)).map(tick => {
+            return [tick.index, tick]
+        })
+    )
+    const swapAmount = new BN(1e6)
+    const tickmap = await market.getTickmap(pair)
+    const {
+        status,
+        accumulatedAmountIn,
+        accumulatedFee,
+        accumulatedAmountOut,
+        amountPerTick,
+    } = simulateSwap({
+        pool,
+        byAmountIn: true,
+        slippage: { v: new BN(0) },
+        swapAmount,
+        xToY,
+        ticks,
+        tickmap
+    })
+    // IN_TOKENS = accumulatedAmountIn + accumulatedFee
+    // OUT_TOKENS = accumulatedAmountOut
+    console.log(status)
+    console.log(`accumulatedAmountIn = ${accumulatedAmountIn.toString()}`)
+    console.log(`accumulatedFee = ${accumulatedFee.toString()}`)
+    console.log(`accumulatedAmountOut = ${accumulatedAmountOut.toString()}`)
+    console.log(`amountPerTick = ${amountPerTick.toString()}`)
 }
 // trunk-ignore(eslint/@typescript-eslint/no-floating-promises)
 main()
