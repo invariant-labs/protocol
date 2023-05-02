2e945514f105053f9664ece7f566d2c564e7ba5b
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-05-02 21:10:49 +0200

    Add trackable_result module to invariant-type

--- programs/invariant/invariant-types/src/utils.rs
+++ programs/invariant/invariant-types/src/utils.rs
@@ -1,60 +1,59 @@
 pub type TrackableResult<T> = Result<T, String>;
 
-#[macro_export]
-macro_rules! error {
-    ($error:expr) => {{
-        Err(format_error(&location!(), $error))
-    }};
-}
+#[macro_use]
+pub mod trackable_result {
 
-#[macro_export]
-macro_rules! ok_or_mark_trace {
-    ($op:expr) => {
-        match $op {
-            Ok(ok) => Ok(ok),
-            Err(err) => Err(trace!(err)),
-        }
-    };
-}
+    #[macro_export]
+    macro_rules! err_msg {
+        ($error:expr) => {{
+            format!(
+                "ERROR CAUSED BY: {}\nINVARIANT STACK TRACE:\n-> {}",
+                $error,
+                &location!()
+            )
+        }};
+    }
 
-macro_rules! function {
-    () => {{
-        fn f() {}
-        fn type_name_of<T>(_: T) -> &'static str {
-            std::any::type_name::<T>()
-        }
-        let name = type_name_of(f);
-        &name[..name.len() - 3]
-    }};
-}
+    #[macro_export]
+    macro_rules! ok_or_mark_trace {
+        ($op:expr) => {
+            match $op {
+                Ok(ok) => Ok(ok),
+                Err(err) => Err(trace!(err)),
+            }
+        };
+    }
 
-macro_rules! location {
-    () => {{
-        format!("{}:{}:{}", file!(), function!(), line!())
-    }};
-}
+    #[macro_export]
+    macro_rules! function {
+        () => {{
+            fn f() {}
+            fn type_name_of<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+            let name = type_name_of(f);
+            &name[..name.len() - 3]
+        }};
+    }
 
-macro_rules! trace {
-    ($error:expr) => {{
-        format_trace(&location!(), $error)
-    }};
-    () => {};
-}
+    #[macro_export]
+    macro_rules! location {
+        () => {{
+            format!("{}:{}:{}", file!(), function!(), line!())
+        }};
+    }
 
-fn format_error(loc: &str, error: &str) -> String {
-    format!(
-        "ERROR CAUSED BY: {}\nINVARIANT STACK TRACE:\n-> {}",
-        error, loc
-    )
-}
-
-fn format_trace(loc: &str, deeper: String) -> String {
-    format!("{} \n-> {}", deeper, loc)
+    #[macro_export]
+    macro_rules! trace {
+        ($deeper:expr) => {{
+            format!("{} \n-> {}", $deeper, &location!())
+        }};
+        () => {};
+    }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     fn value() -> TrackableResult<u64> {
@@ -70,10 +69,8 @@ mod tests {
     }
 
     fn trigger_error() -> TrackableResult<u64> {
-        match outer_fun() {
-            Ok(_) => error!("trigger error"),
-            Err(err) => error!(err.as_str()),
-        }
+        let _ = outer_fun().or_else(|err| Err(err_msg!(err.as_str())))?;
+        Err(err_msg!("trigger error"))
     }
 
     fn inner_fun_err() -> TrackableResult<u64> {
