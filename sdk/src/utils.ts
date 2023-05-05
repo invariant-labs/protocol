b19b08a4eb930c8e0e59b42d6a900eea6ce211fc
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-05-05 15:22:37 +0200

    Update fee tiers

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -278,7 +278,10 @@ export const FEE_TIERS: FeeTier[] = [
   { fee: fromFee(new BN(50)) },
   { fee: fromFee(new BN(100)) },
   { fee: fromFee(new BN(300)) },
-  { fee: fromFee(new BN(1000)) }
+  { fee: fromFee(new BN(1000)) },
+  { fee: new BN(5000), tickSpacing: 5 },
+  { fee: new BN(10000), tickSpacing: 5 },
+  { fee: new BN(50000), tickSpacing: 5 }
 ]
 
 export const generateTicksArray = (start: number, stop: number, step: number) => {
