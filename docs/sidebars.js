fb23d563086a395a6a7fd29790274f1d55718331
Przemyslaw Kulej <przemek.kulej1@gmail.com>
2024-07-03 14:25:05 +0200

    Added tutorial, removed eclipse user guide

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -17,36 +17,36 @@ module.exports = {
         'what_is/glossary'
       ]
     },
-    // {
-    //   type: 'category',
-    //   label: 'Tutorial',
-    //   collapsed: true,
-    //   items: [
-    //     'tutorial/interface',
-    //     'tutorial/priority_fees',
-    //     'tutorial/rpc',
-    //     'tutorial/networks',
-    //     'tutorial/how_to_connect_your_wallet',
-    //     'tutorial/how_to_swap',
-    //     {
-    //       type: 'category',
-    //       label: 'How to add liquidity',
-    //       link: {
-    //         type: 'doc',
-    //         id: 'tutorial/how_to_add_liquidity'
-    //       },
-    //       items: [
-    //         'tutorial/how_to_add_liquidity/uniform_concentration',
-    //         'tutorial/how_to_add_liquidity/price_range'
-    //       ]
-    //     },
-    //     'tutorial/how_to_claim_fee',
-    //     'tutorial/how_to_remove_liquidity',
-    //     'tutorial/stats',
-    //     'tutorial/troubleshooting',
-    //     'tutorial/faq_tutorial'
-    //   ]
-    // },
+    {
+      type: 'category',
+      label: 'Tutorial',
+      collapsed: true,
+      items: [
+        'tutorial/interface',
+        'tutorial/priority_fees',
+        'tutorial/rpc',
+        'tutorial/networks',
+        'tutorial/how_to_connect_your_wallet',
+        'tutorial/how_to_swap',
+        {
+          type: 'category',
+          label: 'How to add liquidity',
+          link: {
+            type: 'doc',
+            id: 'tutorial/how_to_add_liquidity'
+          },
+          items: [
+            'tutorial/how_to_add_liquidity/uniform_concentration',
+            'tutorial/how_to_add_liquidity/price_range'
+          ]
+        },
+        'tutorial/how_to_claim_fee',
+        'tutorial/how_to_remove_liquidity',
+        'tutorial/stats',
+        'tutorial/troubleshooting',
+        'tutorial/faq_tutorial'
+      ]
+    },
     {
       type: 'category',
       label: 'Invariant Bonds',
